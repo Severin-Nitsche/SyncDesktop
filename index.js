5a5b59b60ba05045327359ae881dc181a5537530
Severin <severinnitsche@gmail.com>
2020-03-25 13:25:34 +0100

    Increased Security & fixed sync

--- index.js
+++ index.js
@@ -2,16 +2,38 @@ const electron = require('electron')
 const download = require('./lib/download.js')
 const sync = require('./lib/sync.js')
 const fs = require('fs')
+const reload = require('./lib/reload.js')
+const request = require('request')
 const preferences = require('./preferences/preferences.json')
 const { asJSON } = require('./lib/read.js')
 const { downloadFile } = download;
 const {ipcMain, app, BrowserWindow} = electron
 
-ipcMain.on('sync', async (event, arg) => {
-  console.log(arg)
-  let url = arg.remote+'/download/sync:files.json'
-  let path = 'tmp/files.json'
-  await downloadFile(url, path, () => {gotFile(path, arg, event)})
+ipcMain.on('sync', (event, arg) => {
+  let data = [];
+  reload(arg.directory, data, arg.directory)
+  fs.writeFile(arg.directory+'/files.json', JSON.stringify(data), err => {
+    if(err) throw err
+  })
+  let options = {
+    url: arg.remote+'/login',
+    method: 'POST',
+    headers: {
+      user: arg.user,
+      pwd: arg.password
+    }
+  }
+  request(options, (err, res) => {
+    if(err) {
+      event.reply('status','Failed to connect')
+      return
+    }
+    event.reply('status','connected')
+    let status = res.statusCode
+    event.reply('status',`got Status${status}`)
+    if(status == 403) event.reply('status','Wrong Credentials')
+    else initSync(arg, event, options.headers)
+  });
 })
 
 ipcMain.on('load', (event, arg) => {
@@ -24,20 +46,24 @@ ipcMain.on('preferences', (event, arg) => {
   })
 })
 
-async function gotFile(path, arg, event) {
+async function initSync(arg, event, header) {
+  let url = arg.remote+'/download/sync:files.json'
+  let path = 'tmp/files.json'
+  await downloadFile(url, path, header, () => {gotFile(path, arg, event, header)})
+}
+
+async function gotFile(path, arg, event, header) {
   let remote = await asJSON(path)
-  console.log(remote)
   let local
   try {
     local = await asJSON(arg.directory+'/files.json')
   } catch(e) {
     local = []
   }
-  console.log(local)
   fs.writeFile(arg.directory+'/files.json', JSON.stringify(remote), (err) => {
     if(err) console.log(err)
   })
-  sync(remote, local, arg.remote, arg.directory, event)
+  sync.sync(remote, local, arg.remote, arg.directory, event, header)
 }
 
 function createWindow() {
--- ui/js/client.js
+++ ui/js/client.js
@@ -1,8 +1,8 @@
-ipcRenderer.on('done', (event, arg) => {
+ipcRenderer.on('status', (event, arg) => {
   document.querySelector('#status').innerHTML = arg
 })
 
 function sync() {
   document.querySelector('#status').innerHTML = 'syncing..'
-  ipcRenderer.send('sync', {remote: remote.value, directory: directory.value})
+  ipcRenderer.send('sync', {remote: remote.value, directory: directory.value, user: user.value, password: password.value})
 }
